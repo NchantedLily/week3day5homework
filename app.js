519e0a31f80cdefd9b577afcc5e5c5a9affcd468
Martia <pureromancebymartia@gmail.com>
2022-04-15 15:24:42 -0400

    welcome

---
+++ app.js
@@ -0,0 +1,67 @@
+var a = 100;
+var b =25;
+
+function addSum(param1,param2){
+    console.log(param1 + param2);
+
+}
+
+addSum(a,b);
+
+const addSum = (param1,param2) => {
+    console.log(param1 + param2);
+
+}
+addSum(a,b);
+
+
+
+var e = 10;
+var f =8;
+
+function subtractSum(param1,param2){
+    console.log(param1 - param2);
+
+}
+
+subtractSum(e,f);
+
+const subtractSum = (param1,param2) => {
+    console.log(param1 - param2);
+
+}
+subtractSum(e,f);
+
+
+var c = 5;
+var d =15;
+
+function multiplySum(param1,param2){
+    console.log(param1 * param2);
+
+}
+
+multiplySum(c,d);
+
+const multiplySum = (param1,param2) => {
+    console.log(param1 * param2);
+
+}
+multiplySum(c,d);
+
+
+
+var g = 50;
+var h =5;
+
+function divideSum(param1,param2){
+    console.log(param1 / param2);
+
+}
+divideSum(g,h);
+
+const divideSum = (param1,param2) => {
+    console.log(param1 / param2);
+
+}
+divideSum(g,h);
